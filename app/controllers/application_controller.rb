cec01488d1d58e4092256cf0504749ff38d9d4a9
Xavier Canal i Masjuan <xeviknal@gmail.com>
2018-01-19 15:35:45 +0100

    introducing a bug for demos sake

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_filter :introduce_mistakes
+
+  private
+
+  def introduce_mistakes
+    raise 'random mistake as a service' if (Time.now.to_i % 5).zero?
+  end
 end
